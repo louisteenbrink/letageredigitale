b7940b433462f5b9989784005f63e9d699f2d616
louis steenbrink <louis.steenbrink@gmail.com>
2016-11-02 23:05:57 -0700

    installed node

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
+
+
   def home
     @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
+  get '/posts/:id', to: 'pages#show', :as => :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :forest do
     post '/actions/moderate-comment' => 'comments#moderate_comment'
