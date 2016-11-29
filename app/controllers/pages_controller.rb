8427ffcd4029f05de463ab2ffcdd1a43858a9cf6
louis steenbrink <louis.steenbrink@gmail.com>
2016-11-28 20:16:39 -0800

    prepare for heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,17 @@ class PagesController < ApplicationController
 
   def home
     @posts = Post.all
+    @tags = Tag.all
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def from_tag
+    @posts = Post.joins(:tags).where(tags: { id: params[:tag_id] })
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
   get '/posts/:id', to: 'pages#show', :as => :posts
+  get "/fetch_posts" => 'pages#from_tag', as: 'fetch_posts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :forest do
     post '/actions/moderate-comment' => 'comments#moderate_comment'
   end
+
 end
